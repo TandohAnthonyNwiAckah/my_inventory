ab93ea1ce6ae119032b28850fe3a8a84c926a235
Tandoh Anthony Nwi-Ackah <anthonytandoh90@yahoo.com>
2024-10-04 13:33:30 +0000

    added my inventory app

--- my_inventory_project/my_inventory_project/settings.py
+++ my_inventory_project/my_inventory_project/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'my_inventory_app.apps.MyInventoryAppConfig',
 ]
 
 MIDDLEWARE = [
